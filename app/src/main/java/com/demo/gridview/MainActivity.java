1487d2bd7e3f079b1b68412ab0b4074ff38a8587
timesoftapp <timesoftapp@gmail.com>
2016-12-03 17:18:30 +0900

    2016-12-03 17:16

--- app/src/main/java/com/demo/gridview/MainActivity.java
+++ app/src/main/java/com/demo/gridview/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         gv = (GridView)findViewById(R.id.gv);
-        MyGridAdapter adapter = new MyGridAdapter(this.getApplicationContext());
+        MyGridAdapter adapter = new MyGridAdapter(this);
         gv.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/demo/gridview/MyWebViewClient.java
@@ -0,0 +1,8 @@
+package com.demo.gridview;
+
+/**
+ * Created by 1027 on 2016-12-03.
+ */
+
+public class MyWebViewClient {
+}
